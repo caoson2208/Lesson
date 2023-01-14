b77deba2b32dea122b84c4479ac588411b5f7c53
Cao Ngọc Sơn <caongocson910@gmail.com>
2023-01-14 16:56:26 +0700

    NS commit

--- js/main.js
+++ js/main.js
@@ -18,8 +18,12 @@ $(document).ready(function () {
   $(".images").addClass("wow  animate__fadeInUp animate__delay-0.6s");
   $(".content").addClass("wow animate__fadeInUp animate__delay-0.5s");
 
-  $(".blog-item:nth-child(odd)").addClass("wow animate__slideInLeft animate__delay-0.6s");
-  $(".blog-item:nth-child(even)").addClass("wow animate__slideInRight animate__delay-0.6s");
+  $(".blog-item:nth-child(odd)").addClass(
+    "wow animate__slideInLeft animate__delay-0.6s"
+  );
+  $(".blog-item:nth-child(even)").addClass(
+    "wow animate__slideInRight animate__delay-0.6s"
+  );
   $(".column").addClass("wow animate__fadeInUp animate__delay-0.4s");
   wow = new WOW({
     boxClass: "wow", // default
@@ -39,6 +43,22 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1025, // Độ rộng thiết bị
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false, // Ẩn nút kéo
+          infinite: false, // Kéo tới cuối không kéo dc nữa
+        },
+      },
+    ],
   });
 
   $(".blog-list").slick({
@@ -48,28 +68,40 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1025, // Độ rộng thiết bị
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false, // Ẩn nút kéo
+          infinite: false, // Kéo tới cuối không kéo dc nữa
+        },
+      },
+    ],
   });
 });
 
 /*-----------------------------------
               RUN DATA
 -----------------------------------*/
-var homeSaleTimer = null;
-var homeSoldTimer = null;
-var priceReducedTimer = null;
+var studentTimer = null;
+var coursesTimer = null;
 $(document).ready(function () {
-  var homeSale = $("#sale");
-  var homeSold = $("#sold");
-  var priceReduced = $("#reduced");
+  var students = $("#students");
+  var courses = $("#courses");
 
   $(window).scroll(function () {
-    if ($(this).scrollTop() >= 2600) {
-      if (homeSaleTimer === null)
-        homeSaleTimer = startData(homeSale, 20, 2500, "sale");
-      if (homeSoldTimer === null)
-        homeSoldTimer = startData(homeSold, 40, 5000, "sold");
-      if (priceReducedTimer === null)
-        priceReducedTimer = startData(priceReduced, 2, 170, "reduced");
+    if ($(this).scrollTop() >= 281) {
+      if (studentTimer === null)
+        studentTimer = startData(students, 1, 50, "students");
+      if (coursesTimer === null)
+        coursesTimer = startData(courses, 1, 70, "courses");
     }
   });
 });
@@ -90,6 +122,9 @@ function startData(element, steps, max, type) {
     }, 10);
 }
 
+/*-----------------------------------
+      Change padding on scroll
+-----------------------------------*/
 $(document).ready(function () {
   $(window).on("scroll", function () {
     if ($(this).scrollTop() > 100) $(".header").addClass("header-padding");
